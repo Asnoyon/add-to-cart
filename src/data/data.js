dfcea4bff439da66d6f6aa75654dafc5b61e6dac
Asnoyon <blackpain1100@gmail.com>
2023-07-19 12:05:13 +0600

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            thumb: "./img/2.jpeg",
+            title: "Apple",
+            desc:`Apple`,
+            price:"$45"
+
+        },
+        {
+            id: 2,
+            thumb: "./img/2.jpeg",
+            title: "Apple",
+            desc:`Apple `,
+            price:"$45"
+
+        }
+    ]
+}
+export default data;
